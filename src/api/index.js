e71b7602d5403bf2f65e7affe1ed3e5d369f64a0
Vilin Kehayov <vilinkexayov@gmail.com>
2023-01-20 16:23:06 +0200

    dinamic lat nd lng
    Signed-off-by: Vilin Kehayov <vilinkexayov@gmail.com>

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,25 @@
 import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-const options = {
+
+export const getPlacesData = async(sw, ne) => {
+    try {
+        const {data: {data}}= await axios.get(URL, {
   
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Key': 'a923e9e379msha1d2cd4e73ada32p11e8ffjsnd5cd6c9c49a9',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': 'a923e9e379msha1d2cd4e73ada32p11e8ffjsnd5cd6c9c49a9',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
 
-
-export const getPlacesData = async() => {
-    try{
-        const {data: {data}}= await axios.get(URL, options);
         return data;
-    } catch(error) {
+       } catch (error) {
         console.log(error)
 
     }
