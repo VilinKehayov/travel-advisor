febf60fcef99a91c20957e417eadad27083c2251
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-02-15 16:24:12 +0200

    Started to make the pins clickable

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react';
 import {Paper, Typography, useMediaQuery} from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating'; 
 import useStyles from './styles';
 
 const Map = ({setCoordinates, setBounds, coordinates, places}) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery('(min-width:600px)');
+  const isDesktop = useMediaQuery('(min-width:600px)');
+  const [childClicked, setChildClicked] = useState(null)
   
   return (
     <div className={classes.mapContainer}>
@@ -22,16 +23,23 @@ const Map = ({setCoordinates, setBounds, coordinates, places}) => {
           setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
           setCoordinates({lat: e.center.lat, lng: e.center.lng});
         }}
-        onChildClick={() => {}}
+        onChildClick={(child) => {}}
       >
         {places?.map((place, i)=> (
           <div className={classes.markerContainer} lat={Number(place.lattitude)} Ing={Number(place.longitude)} key={i}>
             {
-              isMobile ? (
+              !isDesktop ? (
                 <LocationOnOutlinedIcon color="primary" fontSize="large"/>
               ) : (
-                <Paper>
-                  
+                <Paper elevation={3} className={classes.paper}>
+                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                      {place.name}
+                    </Typography>
+                    <img className={classes.pointer}
+                          src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                          alt={place.name}
+                         />
+                         <Rating size="small" value={Number(place.rating)} readOnly />
                 </Paper>
               )
             }
